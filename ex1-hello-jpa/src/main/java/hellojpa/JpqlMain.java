27a7fc19cfc0c8f728db26c4a6877c8b2aaeb687
hwkang <landman93@naver.com>
2023-08-09 23:07:05 +0900

    jpa 실습중

--- ex1-hello-jpa/src/main/java/hellojpa/JpqlMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpqlMain.java
@@ -4,9 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
-public class JpaMain {
-
+public class JpqlMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
@@ -16,13 +16,14 @@ public class JpaMain {
         tx.begin();
 
         try {
-            //1. Member 저장
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("HelloJPA");
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)  //paging
+                    .setMaxResults(10)  //paging
+                    .getResultList();
 
-            em.persist(member);
-
+            for(Member member : result) {
+                System.out.println("member.name : " + member.getName());
+            }
             tx.commit();
 
         } catch (Exception e) {
